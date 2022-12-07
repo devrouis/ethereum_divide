07e3cd09e1b4107a5879eb88afd1f1742b7de4bc
devrouis <80670036+devrouis@users.noreply.github.com>
2022-12-06 20:12:15 -0500

    fix

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -21,19 +21,19 @@ import {
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
-  try {
-    const res = await api.get('/profile/me');
+  // try {
+  //   const res = await api.get('/profile/me');
 
-    dispatch({
-      type: GET_PROFILE,
-      payload: res.data
-    });
-  } catch (err) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
-    });
-  }
+  //   dispatch({
+  //     type: GET_PROFILE,
+  //     payload: res.data
+  //   });
+  // } catch (err) {
+  //   dispatch({
+  //     type: PROFILE_ERROR,
+  //     payload: { msg: err.response.statusText, status: err.response.status }
+  //   });
+  // }
 };
 
 // Get all profiles
